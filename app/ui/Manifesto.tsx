98f8c607112eeacbf6432a0b720d0e171bc867ca
Alexei <robohoratio@gmail.com>
2024-01-19 20:58:48 +0200

    fix: build error '

--- app/ui/Manifesto.tsx
+++ app/ui/Manifesto.tsx
@@ -14,7 +14,7 @@ export default function Manifesto() {
                             <li className="pb-4">Skidetika represents the process of transforming opinionated banter in rigorous scientific discussion.</li>
                             <li className="pb-4">Skidetika tries to employ both human subjectivity and robust Bayesian statistical models for accurate probability estimation and future events anticipation. </li>
                             <li className="pb-4"><span className="text-primary">Fritilaria</span> represents the first implementation of <span className="text-fritilariaGreen">skidetics</span> in realm of <span className="text-fritilariaGreen">value investing</span>. </li>
-                            <li><span className="text-primary">Fritilaria</span> performs a <span className="text-fritilariaGreen">skidetik fundamental analysis</span> of any publicly traded stock by employing user's subjective opinion and delivers accurate statistical conclusions. </li>
+                            <li><span className="text-primary">Fritilaria</span> performs a <span className="text-fritilariaGreen">skidetik fundamental analysis</span> of any publicly traded stock by employing user&apos;s subjective opinion and delivers accurate statistical conclusions. </li>
                         </ul>
                     </div>
                 </div>
