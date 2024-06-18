319e443bd97d34bbc38c7d39bc1c481ab3c35ef6
Alexei <robohoratio@gmail.com>
2024-06-18 14:05:32 +0300

    Added initial schema

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,18 @@ import Manifesto from "./ui/Manifesto";
 
 export default function Home() {
 
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'SoftwareApplication',
+    name: 'Skidetica',
+    image: 'https://fortiqrsbyglyzsgzcim.supabase.co/storage/v1/object/public/logo/friti_logo_dark_cropped.png',
+    description: "Transform your opinion into statistically accurate fundamental analysis",
+    publisher: 'Skidetica Labs',
+    category: 'Business',
+    downloadURL: 'htpps://fritilaria.com',
+    operatingSystem: 'Android 11',
+    isAccesibleForFree: true,
+  }
 
   return (
     <div className="flex lg:flex-col h-fit rounded-[25px] min-w-screen lg:w-[600px] xl:w-[840px]">
