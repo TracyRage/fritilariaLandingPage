23effa64a10335562a3a5ffd082abf4f58dad8c2
Alexei <robohoratio@gmail.com>
2024-06-18 14:13:08 +0300

    Minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,17 @@ export default function Home() {
   }
 
   return (
-    <div className="flex lg:flex-col h-fit rounded-[25px] min-w-screen lg:w-[600px] xl:w-[840px]">
-      <div className="flex flex-col lg:space-y-10">
-      <Manifesto/>
-      <Guide/>
+    <section>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <div className="flex lg:flex-col h-fit rounded-[25px] min-w-screen lg:w-[600px] xl:w-[840px]">
+        <div className="flex flex-col lg:space-y-10">
+          <Manifesto />
+          <Guide />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
