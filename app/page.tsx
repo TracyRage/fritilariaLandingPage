4f2363d55d3013ef01afc3cecbd084a422d6dc5a
Alexei <robohoratio@gmail.com>
2024-10-22 18:41:25 +0300

    [fix] rich results | main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,68 +4,10 @@ import Manifesto from "./ui/Manifesto";
 
 export default async function Home() {
 
-  const jsonLd2 = {
 
-    '@context': 'https://schema.org',
-    '@type': 'FAQPage',
-    name: 'Skidetica | FAQ',
-    image: 'https://rmhsfgybzskyoimjlpfe.supabase.co/storage/v1/object/public/logo/friti_logo_dark_cropped_og.png',
-    isAccesibleForFree: true,
-    offers: {
-      '@type': "Offer",
-      price: '0',
-      priceCurrency: 'USD'
-    },
-    softwareVersion: "1.1.0",
-    installURL: 'https://skidetica.com',
-    aggregateRating: {
-      '@type': "AggregateRating",
-      ratingValue: "4.8",
-      ratingCount: "10000"
-    },
-    author: {
-      '@type': "Organization",
-      name: "Skidetica Labs",
-      url: "https://skidetica.com/company",
-      contactPoint: {
-        "@type": "ContactPoint",
-        email: "contanct@skidetica.com"
-      }
-    },
-
-  }
-
-  const jsonLd = {
-
-    '@context': 'https://schema.org',
-    '@type': 'MobileApplication',
-    name: 'Skidetica',
-    image: 'https://rmhsfgybzskyoimjlpfe.supabase.co/storage/v1/object/public/logo/friti_logo_dark_cropped_og.png',
-    description: "Transform your opinion into statistically accurate fundamental analysis",
-    applicationCategory: 'Business',
-    downloadURL: 'https://skidetica.com',
-    operatingSystem: 'Android',
-    isAccesibleForFree: true,
-    softwareVersion: "1.0.0",
-    installURL: 'https://skidetica.com',
-    author: {
-      '@type': "Organization",
-      name: "Skidetica Labs",
-      url: "https://skidetica.com/company",
-      contactPoint: {
-        "@type": "ContactPoint",
-        email: "contact@skidetica.com"
-      }
-    },
-
-  }
 
   return (
     <section>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
       <div className="flex lg:flex-col h-fit rounded-[25px] lg:w-[600px] xl:w-[840px]">
         <div className="flex flex-col lg:space-y-10">
           <div className="hidden lg:block">
